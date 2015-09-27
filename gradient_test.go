6a3de1af45827e0a282bf7c882e8513ccda4f9a9
Peter Hellberg <peter@c7.se>
2015-09-27 14:07:13 +0200

    Added a few more tests

--- gradient_test.go
+++ gradient_test.go
@@ -21,13 +21,20 @@ func TestNewVertical(t *testing.T) {
 		t.Fatalf(`v.Bounds().Dy() = %d, want %d`, got, want)
 	}
 
+	if got, want := v.ColorModel(), color.NRGBAModel; got != want {
+		t.Fatalf(`v.ColorModel() = %v, want %v`, got, want)
+	}
+
 	for i, tt := range []struct {
 		x int
 		y int
 		c color.Color
 	}{
+		{0, 0, color.NRGBA{0, 0, 255, 255}},
 		{100, 100, color.NRGBA{106, 0, 149, 255}},
 		{200, 200, color.NRGBA{213, 0, 42, 255}},
+		{319, 239, color.NRGBA{254, 0, 1, 255}},
+		{900, 900, color.Transparent},
 	} {
 		c := v.At(tt.x, tt.y)
 
@@ -43,6 +50,18 @@ func TestNewVertical(t *testing.T) {
 func TestNewVertical_noStops(t *testing.T) {
 	v := NewVertical(200, 400, []Stop{}).At(100, 150)
 	c := color.NRGBA{255, 0, 255, 255}
+
+	r0, g0, b0, a0 := v.RGBA()
+	r1, g1, b1, a1 := c.RGBA()
+
+	if r0 != r1 || g0 != g1 || b0 != b1 || a0 != a1 {
+		t.Fatalf("v.At(%d, %d) = %+v, want %+v", 200, 400, v, c)
+	}
+}
+
+func TestNewVertical_singleStop(t *testing.T) {
+	c := color.NRGBA{0, 255, 255, 0}
+	v := NewVertical(200, 400, []Stop{{0.0, c}}).At(100, 150)
 
 	r0, g0, b0, a0 := v.RGBA()
 	r1, g1, b1, a1 := c.RGBA()
@@ -69,6 +88,10 @@ func TestNewHorizontal(t *testing.T) {
 		t.Fatalf(`h.Bounds().Dy() = %d, want %d`, got, want)
 	}
 
+	if got, want := h.ColorModel(), color.NRGBAModel; got != want {
+		t.Fatalf(`h.ColorModel() = %v, want %v`, got, want)
+	}
+
 	for i, tt := range []struct {
 		x int
 		y int
@@ -77,6 +100,7 @@ func TestNewHorizontal(t *testing.T) {
 		{0, 0, color.NRGBA{0, 255, 255, 0}},
 		{100, 100, color.NRGBA{0, 177, 0, 177}},
 		{239, 319, color.NRGBA{0, 255, 0, 255}},
+		{900, 900, color.Transparent},
 	} {
 		c := h.At(tt.x, tt.y)
 
@@ -90,13 +114,25 @@ func TestNewHorizontal(t *testing.T) {
 }
 
 func TestNewHorizontal_noStops(t *testing.T) {
-	v := NewHorizontal(200, 400, []Stop{}).At(100, 150)
+	h := NewHorizontal(200, 400, []Stop{}).At(100, 150)
 	c := color.NRGBA{255, 0, 255, 255}
 
-	r0, g0, b0, a0 := v.RGBA()
+	r0, g0, b0, a0 := h.RGBA()
 	r1, g1, b1, a1 := c.RGBA()
 
 	if r0 != r1 || g0 != g1 || b0 != b1 || a0 != a1 {
-		t.Fatalf("v.At(%d, %d) = %+v, want %+v", 200, 400, v, c)
+		t.Fatalf("h.At(%d, %d) = %+v, want %+v", 200, 400, h, c)
+	}
+}
+
+func TestNewHorizontal_singleStop(t *testing.T) {
+	c := color.NRGBA{0, 255, 255, 0}
+	h := NewHorizontal(200, 400, []Stop{{0.0, c}}).At(100, 150)
+
+	r0, g0, b0, a0 := h.RGBA()
+	r1, g1, b1, a1 := c.RGBA()
+
+	if r0 != r1 || g0 != g1 || b0 != b1 || a0 != a1 {
+		t.Fatalf("h.At(%d, %d) = %+v, want %+v", 200, 400, h, c)
 	}
 }
